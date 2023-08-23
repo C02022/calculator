519193ee365427b19d53106bdabbc917c2fdd708
Christopher Obando <chrisobando107@gmail.com>
2023-08-22 23:05:53 -0400

    implement 'operate' function that takes in 2 nums & an operator and uses the functions made previously

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+// num, operator, num - EX: 3+5
+
+let numA;
+let operator;
+let numB;
+
+function operate(operator, numA, numB) {
+
+    this.operators = {
+        "+": add(numA, numB),
+        "-": subtract(numA, numB),
+        "*": multiply(numA, numB),
+        "/": divide(numA, numB)
+    };
+
+    if (!this.operators[operator] || isNaN(numA) || isNaN(numB)) {
+        return NaN;
+    }
+
+    return this.operators[operator];
+}
+
+// console.log(operate(operator, numA, numB));
